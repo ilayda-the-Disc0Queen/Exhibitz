ac4fb4ed8672fa2de8f6d47e6934ce6ab383a50c
rn <raulengo@gmail.com>
2020-06-01 16:55:40 +0100

    more validation added to the booking modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_110101) do
+ActiveRecord::Schema.define(version: 2020_06_01_152907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 2020_06_01_110101) do
     t.integer "card_details"
     t.integer "number_of_tickets"
     t.string "card_holder_name"
-    t.string "expiry_date"
     t.integer "cvc"
+    t.date "expiry_date"
     t.index ["exhibit_id"], name: "index_bookings_on_exhibit_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
