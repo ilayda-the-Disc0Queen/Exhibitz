ae118ed0b365a5b51654cdf4d11b280261b7db9a
Joel Dickens <joeladickens@gmail.com>
2020-06-01 15:48:03 +0100

    favourites

---
+++ app/controllers/favourites_controller.rb
@@ -0,0 +1,24 @@
+class FavouritesController < ApplicationController
+  def index
+    @favourites = Favourite.all.where(user: current_user)
+  end
+
+  def create
+    @favourite = Favourite.new
+    @exhibit = Exhibit.find(params[:exhibit_id])
+    @favourite.exhibit = @exhibit
+    @favourite.user = current_user
+
+    if @favourite.save
+      redirect_to favourites_path
+    else
+      render 'exhibits/index'
+    end
+  end
+
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+    redirect_to favourites_path
+  end
+end
