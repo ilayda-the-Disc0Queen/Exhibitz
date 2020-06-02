a2ee0d3e85baf8f5c609caa58c43bfa4b227a6c4
zita kerepesi <kerepesi.zita@me.com>
2020-06-02 17:25:54 +0100

    removed the login from the main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+
   def default_url_options
     { host: ENV["www.exhibitz.co.uk"] || "localhost:3000" }
   end
